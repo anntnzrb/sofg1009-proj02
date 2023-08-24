864c567f0a1b6acc4832c5b2f7d7e3a59d9e9f52
CristopherVilla20 <cgvilla@espol.edu.ec>
2023-08-24 06:14:15 +0000

    front: Routes home page

---
+++ front/components/Nav2.tsx
@@ -0,0 +1,45 @@
+import LemonIcon from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/lemon-2.tsx";
+
+type Props = {
+  active: string;
+};
+
+export default function Header({ active }: Props) {
+  const menus = [
+    { name: "Ciudades", href: "/" },
+    { name: "Bosques", href: "/components" },
+    { name: "Areas", href: "/docs" },
+  ];
+
+  return (
+    <div class="flex flex-col sm:flex-row gap-4 justify-between items-center max-w-screen-xl mx-auto w-full px-4 sm:px-6 md:px-8 p-4">
+      <div class="text-gray-600 text-center">
+        <span>Tour EC</span>
+      </div>
+      <div class="flex items-center gap-8">
+        <a
+          href="Destinos"
+          class="text-gray-600 hover:underline"
+        >
+         Destinos
+        </a>
+        <a
+          href="/Bosques"
+          class="text-gray-600 hover:underline"
+        >
+          Bosques
+        </a>
+        <a
+          href="/Areas"
+          class="text-gray-600 hover:underline"
+        >
+          Areas Protegitas
+        </a>
+      </div>
+      <div>
+        <h1>Contact us</h1>
+        <p>0988954818</p>
+      </div>
+    </div>
+  );
+}
--- front/routes/index.tsx
+++ front/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from "$fresh/runtime.ts";
-
 import Nav from "../components/Nav.tsx";
+import Nav2 from "../components/Nav2.tsx";
+import Button from "../components/button.tsx";
 import type { Ruta } from "../types.d.ts";
 
 import { h } from "preact";
@@ -30,6 +31,41 @@ export default async function Home() {
           {/* {rutas.map((r) => <h1 class="text-2xl">{r.ciudad}</h1>)} */}
         </div>
       </div>
+      <Nav2 active="/" />
+      <main
+        class="w-full flex px-8 py-10 min-h-[24em] justify-center items-center flex-col gap-8 bg-cover bg-center bg-no-repeat bg-gray-100"
+        style="background-image: url('/static/travelImg.png')"
+      >
+        <h1 class="text-3xl text-white-300 font-bold">Turismo en Ecuador</h1>
+        <div
+          class="bg-white justify-center rounded-xl grid grid-cols-3 gap-4 place-items-center gap-5"
+          style="width:700px; height: 80px;border-radius: 80px;"
+        >
+          <Button style="border-radius: 80px;
+background: var(--navi, #7483BD);
+width: 200px;
+height: 50px;">
+            Turistico
+          </Button>
+          <Button style="border-radius: 80px;
+width: 200px;
+height: 50px;">
+            Bosques
+          </Button>
+          <Button style="border-radius: 80px;
+width: 200px;
+height: 50px;">
+            Areas Protegidas
+          </Button>
+        </div>
+        <Button style="border-radius: 48px;
+background: var(--green, #BCFD49); width: 217px;
+height: 67.005px;">
+          Añadir ruta +
+        </Button>
+        <div class="overflow-y-scroll bg-white w-full min-h-[24em] rounded-2xl">
+        </div>
+      </main>
     </>
   );
 }
